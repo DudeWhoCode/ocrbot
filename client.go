6217c1be64cc194ebb31833cb443deebc9ccc0d8
Naren <narencodes@gmail.com>
2019-08-19 14:00:12 +0530

    Using POST forms is unnecessary after anaconda implementation

--- client.go
+++ client.go
@@ -94,28 +94,3 @@ func subscribeWebhook(envName string) {
 		fmt.Println("Unable to subscribe to WH: ", string(body))
 	}
 }
-
-func SendTweet(tweet string, reply_id string) (*Tweet, error) {
-	fmt.Println("Sending tweet as reply to " + reply_id)
-	//Initialize tweet object to store response in
-	var responseTweet Tweet
-	//Add params
-	params := url.Values{}
-	params.Set("status", tweet)
-	params.Set("in_reply_to_status_id", reply_id)
-	//Grab client and post
-	client := createClient()
-	resp, err := client.PostForm("https://api.twitter.com/1.1/statuses/update.json", params)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	//Decode response and send out
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
-	err = json.Unmarshal(body, &responseTweet)
-	if err != nil {
-		return nil, err
-	}
-	return &responseTweet, nil
-}
