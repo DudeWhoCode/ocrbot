63aa388dc0802f2c9c25615aa2bedf110012e761
Naren <narencodes@gmail.com>
2019-08-20 12:28:40 +0530

    Create a logger instance that writes to both stdout and a file

--- logger.go
+++ logger.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"io"
 	"os"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
 var log = *logrus.New()
 
-func init() {
-	log.Out = os.Stdout
+// newLogger creates a new logger instance that writes to both stdout and a log file
+func newMultiLogger() (*os.File, error) {
+	f, err := os.OpenFile("ocrbot.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, errors.Wrap(err, "create file failed")
+	}
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
 	log.Level = logrus.InfoLevel
+	return f, nil
 }
